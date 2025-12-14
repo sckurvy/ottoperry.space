dabb2b1b9ee141361129e9da71a00a710251685e
Sckurvy <ottoperry743@gmail.com>
2025-12-14 16:02:20 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Discord Webhook Configuration
+
+
+const CONFIG = {
+    DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/1449796892862185603/SoR5A5MhvCIhVBXH6g7R2RjgsDJDhApScsCUIf6gh0tXQPpK1xLEKv8LczlLCwOXlWiW'
+};
+
