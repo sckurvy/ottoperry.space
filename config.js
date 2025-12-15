9e2d268ca4e96cb588c4d29ca800d8a4b7e20fc6
Sckurvy <ottoperry743@gmail.com>
2025-12-14 20:39:22 -0500

    Update Discord webhook URL in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 
 const CONFIG = {
-    DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/1449796892862185603/SoR5A5MhvCIhVBXH6g7R2RjgsDJDhApScsCUIf6gh0tXQPpK1xLEKv8LczlLCwOXlWiW'
+    DISCORD_WEBHOOK_URL: 'https://discord.com/api/webhooks/1449938534181113986/1Uuc2WQVl_jOb6mwC5Q3nD5d4zd0LauRJHvHGoSO8dF5LuKTUPnIGcnnyrwUWfqimmz2'
 };
 
